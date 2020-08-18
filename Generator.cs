8e53fb1b7b3d229b9d1e6aab99f3218234b893b4
Kirk Davis <kirkdavi@amazon.com>
2020-08-17 17:44:01 -0700

    refactored a bit, and added mapping from json types to c# types

--- Generator.cs
+++ Generator.cs
@@ -8,18 +8,18 @@ namespace BindingGenerator
 {
     internal class Generator
     {
-        private JsonDocument _schema;
+        public static JsonDocument Schema;
         private string _schemaClassName;
 
         public void GenerateCodeFiles(string rawSchemaText)
         {
-            _schema = JsonDocument.Parse(rawSchemaText);
+            Schema = JsonDocument.Parse(rawSchemaText);
 
-            _schemaClassName = _schema.RootElement.GetProperty("info").GetProperty("title").GetString();
+            _schemaClassName = Schema.RootElement.GetProperty("info").GetProperty("title").GetString();
 
             Directory.CreateDirectory(_schemaClassName);
 
-            var properties = _schema.RootElement.GetProperty("components").GetProperty("schemas")
+            var properties = Schema.RootElement.GetProperty("components").GetProperty("schemas")
                 .GetProperty("AWSEvent").GetProperty("properties");
 
             WriteClassFile(properties, _schemaClassName);
@@ -28,87 +28,26 @@ namespace BindingGenerator
         private void WriteClassFile(JsonElement properties, string className)
         {
             var sb = new StringBuilder("using System.Text.Json.Serialization;");
-            var fixedClassName = GetMemberName(className);
+            var fixedClassName = Member.GetMemberName(className);
             sb.AppendLine();
             sb.AppendLine();
             sb.AppendLine($"public class {fixedClassName} {{");
 
             foreach (var property in properties.EnumerateObject())
             {
-                var fields = property.Value.EnumerateObject().Select(jp => ValueTuple.Create(jp.Name, jp.Value));
+                var member = new Member(property, fixedClassName);
+                Console.WriteLine(member.ToString());
 
-                var thing = fields.First();
-                var typeName = thing.Item2.GetString();
-
-                if (thing.Item1 == "$ref") {
-                    var childNodePath = thing.Item2.GetString().TrimStart('#', '"');
-                    var childNode = GetElementByPath(childNodePath).GetProperty("properties");
-                    typeName = fixedClassName + GetMemberName(property.Name);
-
-                    WriteClassFile(childNode, typeName);
+                if (member.IsComplexType || member.IsArrayOfComplexType) {
+                    WriteClassFile(member.ChildNode, member.TypeName);
                 }
 
-                sb.AppendLine();
-
-                var propName = GetMemberName(property.Name);
-                if (property.Name.Contains('-'))
-                {
-                    sb.AppendLine($"    [JsonPropertyName(\"{property.Name}\")]");
-                }
-
-                if (typeName == "array")
-                {
-                    var arrayinfo = fields.First(x => x.Item1 == "items");
-
-                    var elementsType = arrayinfo.Item2.EnumerateObject();
-                    if (elementsType.First().Name == "$ref")
-                    {
-                        var childNodePath = elementsType.First().Value.GetString().TrimStart('#', '"');
-                        var childNode = GetElementByPath(childNodePath).GetProperty("properties");
-                        var arrayItemType = GetMemberName(childNodePath.Substring(childNodePath.LastIndexOf('/') + 1));
-
-                        WriteClassFile(childNode, arrayItemType);
-                        typeName = arrayItemType + "[]";
-
-                    } else
-                    {
-                        typeName = arrayinfo.Item2.GetProperty("type").GetString() + "[]";
-                    }
-                }
-
-                sb.AppendLine($"    public {typeName} {propName} {{ get; set; }}");
+                sb.AppendLine(member.ToString());
             }
 
             sb.AppendLine("}");
             Console.WriteLine(sb.ToString());
             File.WriteAllText(Path.Combine(_schemaClassName, $"{className}.cs"), sb.ToString());
-        }
-
-        private JsonElement GetElementByPath(string path)
-        {
-            var propNames = path.Split('/', StringSplitOptions.RemoveEmptyEntries);
-            if (!propNames.Any()) return new JsonElement();
-
-            var je = _schema.RootElement;
-
-            foreach (var propName in propNames)
-            {
-                je = je.GetProperty(propName);
-            }
-
-            return je;
-        }
-
-        private string GetMemberName(string jsonPropName)
-        {
-            var pieces = jsonPropName.Split("-", StringSplitOptions.RemoveEmptyEntries);
-            var sb = new StringBuilder();
-
-            foreach (var piece in pieces) {
-                sb.Append(char.ToUpperInvariant(piece.First()) + piece.Substring(1));
-            }
-            
-            return sb.ToString();
         }
     }
 }
---
+++ Member.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+
+namespace BindingGenerator
+{
+    public class Member
+    {
+        JsonProperty _jprop;
+
+        public Member(JsonProperty jprop, string className)
+        {
+            _jprop = jprop;
+            MemberName = _jprop.Name;
+
+            var fields = jprop.Value.EnumerateObject().Select(jp => new KeyValuePair<string, JsonElement>(jp.Name, jp.Value));
+            var firstKey = fields.First().Key;
+            var firstVal = fields.First().Value;
+
+            IsComplexType = firstKey == "$ref";
+
+            if (firstKey == "type")
+            {
+                //primitive
+                if (firstVal.GetString() == "array")
+                {
+                    //array, get type of items and set as TypeName (with [])
+                    IsArray = true;
+                    var arrayVal = fields.First(x => x.Key == "items").Value;
+                    var elementsType = arrayVal.EnumerateObject();
+                    if (elementsType.First().Name == "$ref")
+                    {
+                        //array items are of complex type
+                        IsArrayOfComplexType = true;
+                        var childNodePath = elementsType.First().Value.GetString().TrimStart('#', '"');
+                        ChildNode = GetElementByPath(childNodePath).GetProperty("properties");
+                        TypeName = GetMemberName(childNodePath.Substring(childNodePath.LastIndexOf('/') + 1));
+                    } else {
+                        TypeName = arrayVal.GetProperty("type").GetString();
+                    }
+
+                } else
+                {
+                    TypeName = firstVal.GetString();
+                }
+            }
+
+            if (firstKey == "$ref")
+            {
+                //complex - create a new class for this type
+                var childNodePath = firstVal.GetString().TrimStart('#', '"');
+                ChildNode = GetElementByPath(childNodePath).GetProperty("properties");
+                TypeName = className + GetMemberName(MemberName);
+            }
+        }
+
+        public string TypeName { get; set; }
+
+        public string MemberName { get; private set; }
+
+        public bool IsArray { get; private set; }
+
+        public bool IsComplexType { get; private set; }
+
+        public bool IsArrayOfComplexType { get; private set; }
+
+        public JsonElement ChildNode { get; private set; }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+
+            if (MemberName.Contains("-"))
+                sb.AppendLine($"    [JsonPropertyName(\"{MemberName}\")]");
+
+            var brackets = IsArray ? "[]" : string.Empty;
+
+            sb.AppendLine($"    public {GetCsharpTypeName()}{brackets} {GetMemberName(MemberName)} {{ get; set; }}");
+
+            return sb.ToString();
+        }
+
+        private JsonElement GetElementByPath(string path)
+        {
+            var propNames = path.Split('/', StringSplitOptions.RemoveEmptyEntries);
+            if (!propNames.Any()) throw new Exception("Element path is not valid!");
+
+            var je = Generator.Schema.RootElement;
+
+            foreach (var propName in propNames)
+            {
+                je = je.GetProperty(propName);
+            }
+
+            return je;
+        }
+
+        private string GetCsharpTypeName()
+        {
+            switch (TypeName)
+            {
+                case "string":
+                    return "string";
+                case "boolean":
+                    return "bool";
+                case "number":
+                    return "float";
+                default:
+                    return TypeName;
+            }
+        }
+
+        public static string GetMemberName(string jsonPropName)
+        {
+            var pieces = jsonPropName.Split("-", StringSplitOptions.RemoveEmptyEntries);
+            var sb = new StringBuilder();
+
+            foreach (var piece in pieces)
+            {
+                sb.Append(char.ToUpperInvariant(piece.First()) + piece.Substring(1));
+            }
+
+            return sb.ToString();
+        }
+    }
+}
